d4255f6e642307847fcf514e08358c9717b53698
Hussnain0341 <109843605+Hussnain0341@users.noreply.github.com>
2024-02-16 15:20:58 +0500

    first changes

---
+++ Lab Tasks/LabTask1.js
@@ -0,0 +1,16 @@
+
+let information={
+    Regno: "FA21-BSE-018",
+    Name: "Muhammad Hussnain",
+    Sem: "6th",
+    Department: "Computer Science",
+    Program: "Software Engineering",
+    GPA:3.34,
+    Address:{
+        City: "Attock",
+        State: "Punjab",
+        Country:"Pakistan"
+    }
+}
+
+console.log(information.Address.Country);
